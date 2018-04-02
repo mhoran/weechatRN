5ae2aaff180a63e7bd337f44ba599cdc438eb51d
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-04-02 21:43:20 +0200

    Change to typescript

--- src/usecase/settings/SettingsList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class SettingsList extends React.Component {
-  render() {
-    return null;
-  }
-}
-
-export default SettingsList;
---
+++ src/usecase/settings/SettingsList.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class SettingsList extends React.Component {
+  render() {
+    return null;
+  }
+}
