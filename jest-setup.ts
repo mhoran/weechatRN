5a2c9822292ae8ce4ac58dfa2068d3b7b4f311d0
Matthew Horan <matt@matthoran.com>
2025-12-06 17:27:32 -0500

    Bump react-native-reanimated to 4.2.1

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,2 +1,7 @@
 import '@testing-library/react-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-worklets', () =>
+  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-return
+  require('react-native-worklets/src/mock')
+);
